e0b36d4604adb199cd9b7e07f2523b50a63b88da
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 04:57:57 +0900

    feat: マッチしたユーザーを管理する用を実装

--- app/redis_instance.py
+++ app/redis_instance.py
@@ -5,6 +5,30 @@ matched_pair_redis: Redis = Redis(
     host="localhost", port=6379, db=0, decode_responses=True
 )
 
+
+class MatchedPair:
+    ex: timedelta = timedelta(hours=6)
+
+    def __init__(self, redis: Redis) -> None:
+        self.r = redis
+
+    def add(self, user_id1: str, user_id2: str) -> None:
+        self.r.set(user_id1, user_id2, ex=self.ex)
+        self.r.set(user_id2, user_id1, ex=self.ex)
+
+    def get(self, user_id: str) -> str | None:
+        pair: str | None = self.r.get(user_id)
+        return pair
+
+    def delete(self, user_id: str) -> None:
+        pair: str = self.r.get(user_id)
+        self.r.delete(user_id)
+        self.r.delete(pair)
+
+
+matched_pair = MatchedPair(matched_pair_redis)
+
+
 finding_match_redis: Redis = Redis(
     host="localhost", port=6379, db=1, decode_responses=True
 )
