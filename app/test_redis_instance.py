3f50ce821cb921f2a3f25fb07335e0455bef1b19
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 04:55:57 +0900

    test: MatchedPairのテスト

--- app/test_redis_instance.py
+++ app/test_redis_instance.py
@@ -1,4 +1,4 @@
-from .redis_instance import PlayingData
+from .redis_instance import MatchedPair, PlayingData
 
 
 class FakeRedis:
@@ -10,6 +10,30 @@ class FakeRedis:
 
     def get(self, key: str) -> str | None:
         return self.store.get(key)
+
+    def delete(self, key: str) -> None:
+        del self.store[key]
+
+
+def test_matched_pair_class() -> None:
+    redis = FakeRedis()
+    matched_pair = MatchedPair(redis)
+
+    user_id1: str = "userid1"
+    user_id2: str = "userid2"
+
+    matched_pair.add(user_id1, user_id2)
+    assert matched_pair.get(user_id1) == user_id2
+    assert matched_pair.get(user_id2) == user_id1
+
+    matched_pair.delete(user_id1)
+    assert matched_pair.get(user_id1) is None
+    assert matched_pair.get(user_id2) is None
+
+    matched_pair.add(user_id1, user_id2)
+    matched_pair.delete(user_id2)
+    assert matched_pair.get(user_id1) is None
+    assert matched_pair.get(user_id2) is None
 
 
 def test_playing_data_class() -> None:
