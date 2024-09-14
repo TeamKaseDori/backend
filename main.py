f702d5131bd9b3952dfdf746e84f3479fff77c23
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-14 17:55:50 +0900

    feat: redisアクセスのためのインスタンスを作成

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from redis import Redis
 
 app = FastAPI()
+matched_pair_redis: Redis = Redis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
+finding_match_redis: Redis = Redis(
+    host="localhost", port=6379, db=1, decode_responses=True
+)
+playing_data_redis: Redis = Redis(
+    host="localhost", port=6379, db=2, decode_responses=True
+)
 
 
 @app.get("/welcome")
