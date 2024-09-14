480e53208a960a8be3d98c1f1283bf6a5b1d6e62
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 07:28:56 +0900

    feat: FindMatch用を実装

--- app/redis_instance.py
+++ app/redis_instance.py
@@ -1,5 +1,7 @@
-from redis import Redis
 from datetime import timedelta
+
+from redis import Redis
+from redis.exceptions import ResponseError
 
 matched_pair_redis: Redis = Redis(
     host="localhost", port=6379, db=0, decode_responses=True
@@ -32,6 +34,74 @@ matched_pair = MatchedPair(matched_pair_redis)
 finding_match_redis: Redis = Redis(
     host="localhost", port=6379, db=1, decode_responses=True
 )
+
+
+class FindMatch:
+    KEY: str = "user_coordinate"
+    EX_LOCK: timedelta = timedelta(seconds=10)
+
+    def __init__(self, redis: Redis) -> None:
+        self.r = redis
+
+    def _perform_lock(self, user_id: str) -> bool:
+        return self.r.set(user_id, 1, nx=True, ex=self.EX_LOCK)
+
+    def _perform_lock_2(self, user_id1: str, user_id2: str) -> bool:
+        can_update1: bool = self._perform_lock(user_id1)
+        can_update2: bool = self._perform_lock(user_id2)
+        if can_update1 and can_update2:
+            return True
+
+        if can_update1:
+            self._unlock(user_id1)
+        if can_update2:
+            self._unlock(user_id2)
+        return False
+
+    def _unlock(self, user_id: str) -> None:
+        self.r.delete(user_id)
+
+    def add(self, user_id: str, longitude: float, latitude: float) -> None:
+        # ロック中は更新しない
+        can_update: bool = self._perform_lock(user_id)
+        if not can_update:
+            return
+
+        self.r.geoadd(
+            self.KEY,
+            [longitude, latitude, user_id],
+        )
+
+        # 更新した後にすぐに探せる状態にしたいため
+        self._unlock(user_id)
+
+    def pop(self, user_id, pair_user_id) -> bool:
+        # ロック中は更新しない
+        # ロックの解除はしない、マッチ成立を伝えている間に追加されたくないため
+        can_update: bool = self._perform_lock_2(user_id, pair_user_id)
+        if not can_update:
+            return False
+
+        self.r.zrem(self.KEY, user_id, pair_user_id)
+        return True
+
+    def find(self, user_id: str, min_m: float, max_m: float) -> list[str]:
+        # user_id が登録されていない可能性がある
+        try:
+            max_range = self.r.geosearch(
+                self.KEY, member=user_id, radius=max_m, unit="m"
+            )
+            min_range = self.r.geosearch(
+                self.KEY, member=user_id, radius=min_m, unit="m"
+            )
+        except ResponseError:
+            return []
+        candidates = list(set(max_range) - set(min_range))
+        return candidates
+
+
+find_match: FindMatch = FindMatch(finding_match_redis)
+
 
 playing_data_redis: Redis = Redis(
     host="localhost", port=6379, db=2, decode_responses=True
