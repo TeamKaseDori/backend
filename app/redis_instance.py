e10b19787aa4a48d0cff0142c7dfd87968d06d49
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 04:29:17 +0900

    feat: PlayingDataを扱うクラスを実装

--- app/redis_instance.py
+++ app/redis_instance.py
@@ -11,3 +11,28 @@ finding_match_redis: Redis = Redis(
 playing_data_redis: Redis = Redis(
     host="localhost", port=6379, db=2, decode_responses=True
 )
+
+
+class PlayingData:
+    # divider
+    D: str = ","
+
+    def __init__(self, redis: Redis) -> None:
+        self.r = redis
+
+    def set(self, user_id: str, longitude: float, latitude: float) -> None:
+        key = user_id
+        value = f"{longitude}{self.D}{latitude}"
+        self.r.set(key, value)
+
+    def get(self, user_id: str) -> (float, float, bool):
+        value: str | None = self.r.get(user_id)
+        if not value:
+            return (0, 0, False)
+        values: list[str] = value.split(self.D)
+        longitude: float = float(values[0])
+        latitude: float = float(values[1])
+        return (longitude, latitude, True)
+
+
+playing_data = PlayingData(playing_data_redis)
