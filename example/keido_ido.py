c581b97ed8611c1eb64e2379ff5e3c894a04eabc
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 00:39:26 +0900

    docs: exampleがエラーにならないように修正

--- example/keido_ido.py
+++ example/keido_ido.py
@@ -1,27 +1,50 @@
-from fastapi import FastAPI, WebSocket
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, WebSocket, WebSocketException, status
+from pydantic import BaseModel, Field, ValidationError
+from redis import Redis
+
+from app.auth import TokenData, get_user_info
 
 app = FastAPI()
 
-r = Redis()
+
+playing_data_redis: Redis = Redis(
+    host="localhost", port=6379, db=10, decode_responses=True
+)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+class PlayData(BaseModel):
+    # 経度
+    longitude: float = Field(title="keido")
+    # 緯度
+    latitude: float = Field(title="ido")
+    # 中断
+    abort: bool = Field(default=False, title="abort")
+
+
+# TODO: websocket を終了するときは `matched_pair_redis` から削除
+@app.websocket("/playing")
+async def playing(
+    websocket: WebSocket, token_data: Annotated[TokenData, Depends(get_user_info)]
+):
+    user_id: str = token_data.user_id
+
     await websocket.accept()
     while True:
         # data には 相手のuser_id と 自分の座標
-        data = await websocket.receive_text()
+        data = await websocket.receive_json()
+        try:
+            play_data: PlayData = PlayData.model_validate(**data)
+        except ValidationError:
+            raise WebSocketException(code=status.WS_1007_INVALID_FRAME_PAYLOAD_DATA)
 
         # 自分の緯度・経度を登録
-        user_id = data["user_id"]
-        loc = {
-            "ido": data["ido"],
-            "keido": data["keido"],
-        }
-        r.json().set(user_id, "$", loc)
+        playing_data_redis.set(user_id, play_data.longitude)
 
         # 相手の経度・緯度を取得
-        other_user_id = data["other_user_id"]
-        others_keido_ido = r.json().get(other_user_id, "$")
+        # `mathed_pair_redis` から取得
+        # others_keido_ido = playing_data_redis.get("a")
+        other_play_data = PlayData()
 
-        await websocket.send_text(f"data: {others_keido_ido}")
+        await websocket.send_json(other_play_data.model_json_schema)
