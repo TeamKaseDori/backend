dfb7ae94705c5486ccc70f9af49a195a67215ee9
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 08:15:10 +0900

    chore: コードの位置を変更
    散らばっていたため

--- app/redis_instance.py
+++ app/redis_instance.py
@@ -7,6 +7,14 @@ matched_pair_redis: Redis = Redis(
     host="localhost", port=6379, db=0, decode_responses=True
 )
 
+finding_match_redis: Redis = Redis(
+    host="localhost", port=6379, db=1, decode_responses=True
+)
+
+playing_data_redis: Redis = Redis(
+    host="localhost", port=6379, db=2, decode_responses=True
+)
+
 
 class MatchedPair:
     ex: timedelta = timedelta(hours=6)
@@ -26,14 +34,6 @@ class MatchedPair:
         pair: str = self.r.get(user_id)
         self.r.delete(user_id)
         self.r.delete(pair)
-
-
-matched_pair = MatchedPair(matched_pair_redis)
-
-
-finding_match_redis: Redis = Redis(
-    host="localhost", port=6379, db=1, decode_responses=True
-)
 
 
 class FindMatch:
@@ -100,14 +100,6 @@ class FindMatch:
         return candidates
 
 
-find_match: FindMatch = FindMatch(finding_match_redis)
-
-
-playing_data_redis: Redis = Redis(
-    host="localhost", port=6379, db=2, decode_responses=True
-)
-
-
 class PlayingData:
     # divider
     D: str = ","
@@ -130,4 +122,6 @@ class PlayingData:
         return (longitude, latitude, True)
 
 
-playing_data = PlayingData(playing_data_redis)
+matched_pair: MatchedPair = MatchedPair(matched_pair_redis)
+find_match: FindMatch = FindMatch(finding_match_redis)
+playing_data: PlayingData = PlayingData(playing_data_redis)
