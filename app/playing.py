25bb4b0957d66e050bf4f7e821ae97a5d2b0e3b7
Your Name <144820358+tetracontinental@users.noreply.github.com>
2024-09-15 11:56:25 +0900

    playing関数のみ

--- app/playing.py
+++ app/playing.py
@@ -1,18 +1,41 @@
 from fastapi import APIRouter, WebSocket
 from .redis_instance import playing_data_redis
+import json
 
 router = APIRouter()
 
 
 @router.websocket("/playing")
 async def playing(websocket: WebSocket) -> None:
-    await websocket.accept()
+    await websocket.accept()  # WebSocket接続の受け入れ
 
     while True:
-        data = await websocket.receive_text()
+        try:
+            # クライアントからメッセージを受け取る
+            data = await websocket.receive_text()
 
-    playing_data_redis.set("a", "b")
-    pass
+            message = json.loads(data)
+
+            # 緯度と経度を取得
+            latitude = message.get("latitude")
+            longitude = message.get("longitude")
+
+            if latitude is not None and longitude is not None:
+                # Redisに緯度と経度を保存
+                playing_data_redis.set("latitude", latitude)
+                playing_data_redis.set("longitude", longitude)
+
+                # クライアントに緯度と経度を送信
+                data = {"latitude": latitude, "longitude": longitude}
+                await websocket.send_json(data)
+        except Exception as e:
+            # エラーが発生した場合の処理
+            await websocket.send_text(f"An error occurred: {str(e)}")
+            break
+
+    # WebSocket接続の終了
+    await websocket.close()
+
 
 
 @router.get("/item")
