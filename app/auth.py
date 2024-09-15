689713928595cba0561636639ebc777a0abfa6f0
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 15:01:53 +0900

    Revert "feat: CORSに関するヘッダーを追加"
    This reverts commit 1f2f1cdcd7ccfef268ba5c8e171851270ed716e2.

--- app/auth.py
+++ app/auth.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 from typing import Annotated, Any
 
 import jwt
-from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jwt.exceptions import InvalidTokenError
 from pydantic import BaseModel
@@ -37,9 +37,7 @@ def get_user_info(
 
 
 @router.get("/register")
-def register(response: Response) -> dict:
-    response.headers["Access-Control-Allow-Origin"] = "*"
-
+def register() -> dict:
     expire = datetime.now(timezone.utc) + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     user_id: str = str(uuid.uuid4())
     claimes: dict[str, Any] = {
