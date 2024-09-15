afbf29c42ac38e8463847bb97e424838ddc6966e
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-15 12:42:17 +0900

    chore: redis接続テスト

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
 from app import auth, matching, playing
+from redis import Redis
 
 app: FastAPI = FastAPI()
 app.include_router(auth.router)
 app.include_router(matching.router)
 app.include_router(playing.router)
 
+HOST = "red-crj4ijij1k6c73fjhnsg"
+
+r: Redis = Redis(host=HOST, port=6379, db=8, decode_responses=True)
+r.set("a", "hello")
+
 
 @app.get("/welcome")
 async def welcome() -> dict:
-    return {"message": "welcome"}
+    return {"message": r.get("a")}
